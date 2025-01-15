fc0630daee564cfd87f2a77d74e28780a1266ed6
Bishwas Adhikari <147196562+bishwasadh@users.noreply.github.com>
2025-01-15 11:11:50 -0500

    class activity updated

--- JavaScript/Intro-to-Javascript/intro.js
+++ JavaScript/Intro-to-Javascript/intro.js
@@ -1,2 +1,31 @@
 console.log("Hello, World!");
- 
+
+//Data Types
+// var num = 5; // General declaration, previously was used ot declare scopes
+// let num1 = 5; //General declaration, variable takes on current scope
+// const num2 = 5; // declaring constants, cannot be changed after
+
+//Boolean - true/false
+//null - empty value
+//undefined - variable has been declared but not assigned a value   
+//Number - integer, float, etc
+//string - text(string of characters)
+//symbol - unique value, used for object properties
+
+
+// Expressions, Operators
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
+
+
+console.log(num1 != num2); // true
+console.log(num1== num2 && num2!=num3); // false
+
+
+
+// arrays, objects
+// loops, conditionals
+// functions 
+// Classes
+// DOM; Document Object Model
