2cd84a1e260e935c35a3cadb59decfab443488f7
Bishwas Adhikari <147196562+bishwasadh@users.noreply.github.com>
2025-02-03 21:49:31 -0500

    Added the JavaScript code to inject the content for the Product Page. Renamed assests folder to assets

--- Assignments/Assignment_1/scripts/app.js
+++ Assignments/Assignment_1/scripts/app.js
@@ -6,4 +6,29 @@ document.addEventListener("DOMContentLoaded", () => {
         </div>
     `;
     document.body.insertAdjacentHTML('afterbegin', homeContent);
+
+    // Products Page Content
+    const productContent = `
+        <div class="container mt-5">
+            <h2>Our Favorite Media</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="assets/media1.jpg" class="img-fluid" alt="Media 1">
+                    <p>Media 1 description goes here. This is an interesting piece of media that we highly recommend.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="assets/media2.jpg" class="img-fluid" alt="Media 2">
+                    <p>Media 2 description goes here. This is another great piece of media that you should check out.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="assets/media3.jpg" class="img-fluid" alt="Media 3">
+                    <p>Media 3 description goes here. This is a must-see piece of media that you will enjoy.</p>
+                </div>
+            </div>
+        </div>
+    `;
+    document.querySelector('.nav-link[href="#Products"]').addEventListener('click', () => {
+        document.body.innerHTML = productContent;
+    });
+
 });
