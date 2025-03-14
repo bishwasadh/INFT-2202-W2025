09da9a51195006b4610704160755c9e9f5ef9a91
Bishwas Adhikari <147196562+bishwasadh@users.noreply.github.com>
2025-03-14 17:53:52 -0400

    Add User class for storing user information

--- Assignments/Assignment_2/scripts/app.js
+++ Assignments/Assignment_2/scripts/app.js
@@ -8,6 +8,21 @@
 
 // IIFE - Immediately Invoked Function Expression
 (function(){
+
+
+    /**
+     * User Class for storing user information
+     */
+    class User {
+        // Constructor
+        constructor(firstName, lastName, email, password) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.username = firstName.toLowerCase() + lastName.toLowerCase();
+            this.email = email;
+            this.password = password;
+        }
+    }
     
     /**
      * This function initializes the application
